ca83633efd5387088bd829efcd43b5d1d6259fc5
Clarence Gerobin <129592471+Clarence-Gerobin@users.noreply.github.com>
2023-07-07 11:02:44 -0700

    Add project files.

---
+++ PILING_PELIKULA - EVOLUTION/Movie_Info.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilingPelikula;
+
+public class Movie_Info
+{
+    public string MovieName, MovieDirector;
+    public string MovieSynopsis, MovieCast;
+    public int MovieRelease, MovieRuntime;
+
+    public Movie_Info(string MovieName, int MovieRelease, string Director, string MovieSynopsis, string MovieCast, int Runtime)
+    {
+        this.MovieName = MovieName;
+        this.MovieRelease = MovieRelease;
+        this.MovieDirector = Director;
+        this.MovieSynopsis = MovieSynopsis;
+        this.MovieCast = MovieCast;
+        this.MovieRuntime = Runtime;
+    }
+
+    public void Show_Detail()
+    {
+        Console.WriteLine("Title    : " + MovieName);
+    }
+
+    public void Show_Details()
+    {
+        Console.WriteLine("Release Date : " + MovieRelease);
+        Console.WriteLine("Director     : " + MovieDirector);
+        Console.WriteLine("Synopsis     : " + MovieSynopsis);
+        Console.WriteLine("Main Cast    : " + MovieCast);
+        Console.WriteLine("Duration     : " + MovieRuntime + "minutes");
+    }
+
+}
+
---
+++ PILING_PELIKULA - EVOLUTION/Recommendation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PilingPelikula;
+
+public class Recommendation
+{ 
+    public string movieName;
+    public string movieGenre;
+    public string movieSynopsis;
+}
+
